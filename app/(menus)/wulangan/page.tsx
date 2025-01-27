c5cca85fb11df88539db2e5499dbadaec1c1a0a5
rezadrian01 <ahmadadrian324@gmail.com>
2025-01-27 19:20:45 +0700

    feat: create wulangan page

--- app/(menus)/wulangan/page.tsx
+++ app/(menus)/wulangan/page.tsx
@@ -13,29 +13,115 @@ const page = () => {
     console.log("video clicked");
   };
 
+  const handleBackClick = () => {
+    console.log("back clicked");
+  };
+
   return (
-    <div className="h-screen">
-      <div className="flex flex-col justify-between w-[7rem]">
-        <CircleButton onClick={handleFigureClick}>
+    <div className="relative h-screen w-full">
+      <div className="flex h-full justify-between items-center max-w-[60rem] mx-auto">
+        <div className="flex flex-col gap-44 justify-between w-full text-center">
+          <div className="w-[15rem] flex flex-col justify-center items-center">
+            <CircleButton onClick={handleFigureClick}>
+              <Image
+                className="aspect-square w-[5rem]"
+                src="head-figure.svg"
+                alt="head-figure"
+                width={500}
+                height={500}
+              />
+            </CircleButton>
+            <h3 className="font-semibold text-xl mt-4">Teks Profile Tokoh</h3>
+          </div>
+          <div className="w-[15rem] flex flex-col justify-center items-center">
+            <CircleButton onClick={handleVideoClick}>
+              <Image
+                className="aspect-square w-[5rem]"
+                src="record.svg"
+                alt="record-logo"
+                width={500}
+                height={500}
+              />
+            </CircleButton>
+            <h3 className="font-semibold text-xl mt-4">Wulanagan Video</h3>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 min-w-[40rem] ">
+          <div className="relative w-[40rem]">
+            <Image
+              className="w-[40rem]"
+              src="bubble-chat-1.svg"
+              alt="bubble-chat"
+              width={500}
+              height={500}
+            />
+            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-1/2 left-1/2 font-semibold text-2xl w-9/12">
+              Assalamualaikum bocah-bocah! Kepriye kabare? Ing kene awake dhewe
+              bakal ngrembug babagan teks profil tokoh. Ana sing weruh apa iku
+              teks profil tokoh? Kepriye carane nulis teks profil tokoh kuwi?
+            </p>
+          </div>
+
+          <div className="relative">
+            <Image
+              className="w-[40rem]"
+              src="bubble-chat-2.svg"
+              alt="bubble-chat"
+              width={500}
+              height={500}
+            />
+            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-[35%] left-1/2 font-semibold text-2xl w-9/12">
+              Ana ing sisih kiwa iki materi teks profil tokoh kang kudu
+              digatekake! Ayo sinau bebarengan!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Absolute  elements */}
+
+      {/* Figure image */}
+      <div className="absolute bottom-0 right-0 -z-10">
+        <Image
+          className="w-[15rem]"
+          src="woman-figure-1.svg"
+          alt="woman-figure"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      {/* Cloud elements */}
+      <div className="absolute left-0 top-[30%] -translate-y-1/2 -z-10">
+        <Image
+          className="max-w-[25rem]"
+          src="left-cloud.svg"
+          alt="cloud"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      <div className="absolute right-0 top-1/2 -translate-y-1/2 -z-10">
+        <Image
+          className="max-w-[10rem]"
+          src="right-cloud.svg"
+          alt="cloud"
+          width={500}
+          height={500}
+        />
+      </div>
+
+      {/* Back Button */}
+      <div className="absolute bottom-10 left-10">
+        <CircleButton onClick={handleBackClick}>
           <Image
-            className="aspect-square w-[5rem]"
-            src="head-figure.svg"
-            alt="head-figure"
+            className="aspect-square w-[3rem]"
+            src="back.svg"
+            alt="back-logo"
             width={500}
             height={500}
           />
         </CircleButton>
-        <div>
-          <CircleButton onClick={handleVideoClick}>
-            <Image
-              className="aspect-square"
-              src="record.svg"
-              alt="record-logo"
-              width={500}
-              height={500}
-            />
-          </CircleButton>
-        </div>
       </div>
     </div>
   );

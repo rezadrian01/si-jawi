142da40c6d78da51ee1b8da5aec7a0585a79c9ba
FathanAlfariel <faril191204@gmail.com>
2025-01-29 09:21:41 +0700

    feat: create animation for capaian-pasinaonan page

--- app/(menus)/capaian-pasinaonan/page.tsx
+++ app/(menus)/capaian-pasinaonan/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import React from 'react'
 import BackButton from "../components/back-button"
+import { motion } from "framer-motion";
+import {
+  DEFAULT_FADE_IN_OUT_ANIMATION,
+  LEFT_CLOUD_ANIMATION,
+  RIGHT_CLOUD_ANIMATION,
+} from "../lib/animations";
 
 const CapaianPasinaonanPage = () => {
   return (
     <>
         {/* Cloud elements */}
-        <div className="absolute left-0 top-[30%] -translate-y-1/2 -z-10">
+        <motion.div
+            variants={LEFT_CLOUD_ANIMATION}
+            initial="hidden"
+            animate="visible"
+            exit="exit" 
+            className="absolute left-0 top-[20%] -translate-y-1/2 -z-10">
             <Image
                 className="max-w-[25rem]"
                 src="/left-cloud.svg"
@@ -14,9 +27,14 @@ const CapaianPasinaonanPage = () => {
                 width={500}
                 height={500}
             />
-        </div>
+        </motion.div>
 
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 -z-10">
+        <motion.div
+            variants={RIGHT_CLOUD_ANIMATION}
+            initial="hidden"
+            animate="visible"
+            exit="exit" 
+            className="absolute right-0 top-[40%] -translate-y-1/2 -z-10">
             <Image
                 className="max-w-[10rem]"
                 src="/right-cloud.svg"
@@ -24,10 +42,15 @@ const CapaianPasinaonanPage = () => {
                 width={500}
                 height={500}
             />
-        </div>
+        </motion.div>
 
         {/* Main Content */}
-        <div className="min-h-screen min-w-full flex justify-center items-center">
+        <motion.div
+            variants={DEFAULT_FADE_IN_OUT_ANIMATION()}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="min-h-screen min-w-full flex justify-center items-center">
             <div className="p-8 pb-16 w-full max-w-4xl border-8 border-black flex flex-col items-center mx-auto rounded-[50px] shadow-lg">
                 <h6 className="text-xl font-bold text-center">Capaian Pasinaonan<br/>Elemen Berbicara dan Memirsa</h6>
 
@@ -36,10 +59,10 @@ const CapaianPasinaonanPage = () => {
                     Peserta didik menginterpretasikan informasi untuk mengungkapkan simpati,kepedulian, empati, atau pendapat pro dan kontra secara santun (unggah-ungguh/tata krama) dari teks visual. Peerta didik mampu mengeksplorasi dan mengevaluasi berbagai topik aktual yang dibaca dan dipirsa.
                 </p>
             </div>
-        </div>
+        </motion.div>
 
         {/* Back Button */}
-        <BackButton />
+        <BackButton animate />
     </>
   )
 }

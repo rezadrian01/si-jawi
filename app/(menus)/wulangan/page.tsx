96a0f477e8a56557dd31e061e2684b1b2a94de39
rezadrian01 <ahmadadrian324@gmail.com>
2025-02-02 10:05:23 +0700

    style: update mobile views on main wulangan page

--- app/(menus)/wulangan/page.tsx
+++ app/(menus)/wulangan/page.tsx
@@ -28,8 +28,8 @@ const WulanganPage = () => {
 
   return (
     <div className="relative h-screen w-full overflow-hidden">
-      <div className="flex h-full justify-between items-center max-w-[60rem] mx-auto">
-        <div className="flex flex-col gap-44 justify-between w-full text-center">
+      <div className="flex flex-col gap-2 lg:flex-row h-full justify-center lg:justify-between items-center max-w-[60rem] mx-auto border border-black">
+        <div className="flex lg:flex-col gap-20 lg:gap-44 justify-between w-full text-center">
           <motion.div
             variants={DEFAULT_FADE_IN_OUT_ANIMATION(0.25)}
             initial="hidden"
@@ -39,14 +39,16 @@ const WulanganPage = () => {
           >
             <CircleButton onClick={handleFigureClick}>
               <Image
-                className="aspect-square w-[5rem] object-cover"
+                className="aspect-square w-[3rem] md:w-[5rem] object-cover"
                 src="/images/head-figure.png"
                 alt="head-figure"
                 width={500}
                 height={500}
               />
             </CircleButton>
-            <h3 className="font-semibold text-xl mt-4">Teks Profile Tokoh</h3>
+            <h3 className="font-semibold text-xs md:text-xl mt-4">
+              Teks Profile Tokoh
+            </h3>
           </motion.div>
           <motion.div
             variants={DEFAULT_FADE_IN_OUT_ANIMATION(0.25)}
@@ -57,32 +59,34 @@ const WulanganPage = () => {
           >
             <CircleButton onClick={handleVideoClick}>
               <Image
-                className="aspect-square w-[5rem]"
+                className="aspect-square w-[3rem] md:w-[5rem]"
                 src="/record.svg"
                 alt="record-logo"
                 width={500}
                 height={500}
               />
             </CircleButton>
-            <h3 className="font-semibold text-xl mt-4">Wulanagan Video</h3>
+            <h3 className="font-semibold text-xs md:text-xl mt-4">
+              Wulanagan Video
+            </h3>
           </motion.div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-4 min-w-[40rem] ">
+        <div className="flex flex-col items-center justify-center gap-6 md:gap-4 min-w-[40rem]">
           <motion.div
             variants={DEFAULT_FADE_IN_OUT_ANIMATION(0.25)}
             initial="hidden"
             animate="visible"
             exit="exit"
-            className="relative w-[40rem] -mr-[20rem]"
+            className="relative w-[40rem] -mr-[23rem] md:-mr-[20rem]"
           >
             <Image
-              className="w-[40rem]"
+              className="w-[20rem] md:w-[35rem]"
               src="/bubble-chat-1.svg"
               alt="bubble-chat"
               width={500}
               height={500}
             />
-            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-1/2 left-1/2 font-semibold text-2xl w-9/12">
+            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-1/2 left-1/4 md:left-[45%] font-semibold text-xs md:text-2xl w-4/12 md:w-8/12">
               Assalamualaikum bocah-bocah! Kepriye kabare? Ing kene awake dhewe
               bakal ngrembug babagan teks profil tokoh. Ana sing weruh apa iku
               teks profil tokoh? Kepriye carane nulis teks profil tokoh kuwi?
@@ -94,16 +98,16 @@ const WulanganPage = () => {
             initial="hidden"
             animate="visible"
             exit="exit"
-            className="relative"
+            className="relative -ml-[5rem] md:-ml-[10rem] lg:-ml-[5rem]"
           >
             <Image
-              className="w-[40rem]"
+              className="w-[17rem] md:w-[35rem] lg:w-[40rem]"
               src="/bubble-chat-2.svg"
               alt="bubble-chat"
               width={500}
               height={500}
             />
-            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-[35%] left-1/2 font-semibold text-2xl w-9/12">
+            <p className="absolute z-20 -translate-x-1/2 -translate-y-1/2 text-center top-[35%] left-1/2 font-semibold text-xs md:text-2xl w-9/12">
               Ana ing sisih kiwa iki materi teks profil tokoh kang kudu
               digatekake! Ayo sinau bebarengan!
             </p>
@@ -121,7 +125,7 @@ const WulanganPage = () => {
         className="absolute bottom-0 right-0 -z-10"
       >
         <Image
-          className="w-[15rem]"
+          className="w-[7.5rem] md:w-[15rem]"
           src="/images/woman-figure-1.png"
           alt="woman-figure"
           width={500}
@@ -138,7 +142,7 @@ const WulanganPage = () => {
         className="absolute left-0 top-[20%] -translate-y-1/2 -z-10"
       >
         <Image
-          className="max-w-[25rem]"
+          className="max-w-72 md:max-w-[25rem]"
           src="/left-cloud.svg"
           alt="cloud"
           width={500}
@@ -154,7 +158,7 @@ const WulanganPage = () => {
         className="absolute right-0 top-[40%] -translate-y-1/2 -z-10"
       >
         <Image
-          className="max-w-[10rem]"
+          className="max-w-20 md:max-w-[10rem]"
           src="/right-cloud.svg"
           alt="cloud"
           width={500}
